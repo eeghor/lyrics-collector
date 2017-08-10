3d49dff8ab0888a11af3c0dce14ff9c4f9b9ffd1
Unknown <eeghor@gmail.com>
2017-08-10 17:06:53 +1000

    successfully gets lyrics

--- genius_lyrics.py
+++ genius_lyrics.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 ACCESS_TOKEN = "xbe3FTL4S0Ybni7r6sPaKFYSkcVVszqPWoGAQUZw_kSBYFjTkLBFHieDULkq2AO3"
 
@@ -10,10 +11,21 @@ SEARCH_URL = BASE_URL + "/search"
 
 headers = {'Authorization': TOKEN}
 
-ARTIST = "porcupine tree"
+ARTIST = "depeche mode"
 
-response = requests.get(SEARCH_URL, data={"q": "porcupine"}, headers=headers)
+response = requests.get(SEARCH_URL, data={"q": "personal jesus"}, headers=headers)
 
 for hit in response.json()["response"]["hits"]:
 	if hit["result"]["primary_artist"]["name"].lower() == ARTIST:
-		print(hit["result"]["api_path"])    # this path is like "/songs/65262"
+		song_api = hit["result"]["api_path"]    # this path is like "/songs/65262"
+		song_url = BASE_URL + song_api
+		response = requests.get(song_url, headers=headers)
+		path = response.json()["response"]["song"]["path"]
+		page_url = "http://genius.com" + path
+		page = requests.get(page_url)
+		html = BeautifulSoup(page.text, "html.parser")
+		# PageElement.extract() removes a tag or string from the tree. 
+		# It returns the tag or string that was extracted:
+		[h.extract() for h in html('script')]
+		lyrics = html.find("div", class_="lyrics").get_text()
+		print(lyrics)
